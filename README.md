fcb28acc6f12bc9f1eb818cc493e860c58ef667a
SylTi <syltilt@gmail.com>
2014-12-22 00:55:38 +0100

    first commit with ui-map

---
+++ README.md
@@ -0,0 +1,5 @@
+cordova run --target="AVD_for_Nexus_4_by_Google"
+
+ionic serve
+
+cordova plugin add org.apache.cordova.geolocation
---
+++ www/js/controllers.js
@@ -0,0 +1,36 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  }
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('FriendsCtrl', function($scope, Friends) {
+  $scope.friends = Friends.all();
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.friendId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+})
+
+.controller('MapCtrl', function($scope) {
+  $scope.mapOptions = {
+    center: new google.maps.LatLng(35.784, -78.670),
+    zoom: 15,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+});
